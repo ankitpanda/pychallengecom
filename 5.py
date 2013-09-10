f0928a67d790ef3b09bb3a946ac19f2e4c2a1a17
Ankit Panda <ankit@ankitpanda.com>
2013-09-10 00:18:54 -0400

    Renamed some things

--- 5.py
+++ 5.py
@@ -1,5 +1,6 @@
-import urllib,pickle
-url='http://www.pythonchallenge.com/pc/def/banner.p'
-obj=pickle.load(urllib.urlopen(url))
-for line in obj:
-    print ''.join(map(lambda pair: pair[0]*pair[1], line))
+import pickle, urllib
+
+url = 'http://www.pythonchallenge.com/pc/def/banner.p'
+pickled = pickle.load(urllib.urlopen(url))
+for line in pickled:
+    print ''.join(map(lambda pair: pair[0] * pair[1], line))
